da35530a5181dbf11188bd919bfd97e771f1184a
Katherine Wang <kathylu.wang@gmail.com>
2021-12-02 09:30:37 -0500

    Update game title to SlinkyCat.

--- main.cpp
+++ main.cpp
@@ -67,7 +67,7 @@ int main(int argc, char **argv) {
 
 	//create window:
 	SDL_Window *window = SDL_CreateWindow(
-		"Slinky", //TODO: remember to set a title for your game!
+		"SlinkyCat", //TODO: remember to set a title for your game!
 		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
 		1280, 720, //TODO: modify window size if you'd like
 		SDL_WINDOW_OPENGL
